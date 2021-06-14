25dbe6350cbefe6de71afda4f68df3fab722ef0d
casualbill <hotwindzb@gmail.com>
2021-06-14 21:42:23 +0800

    Optimize redis methods to add close connection

--- redis.js
+++ redis.js
@@ -1,18 +1,22 @@
 const redis = require('redis');
 const { promisify } = require('util');
 
-const client = redis.createClient();
-const exists = promisify(client.exists).bind(client);
-
 const EXPIRE_TIME = 259200; // Expire time for recycling unused room
 
-client.on('error', (err) => {
-  console.error(`Redis error: ${err}`);
+const redisHandler = new Promise((resolve) => {
+  const client = redis.createClient();
+  client.on('connect', () => {
+    console.log('Redis coneected');
+    resolve(client);
+  });
+  client.on('error', (err) => {
+    console.error(`Redis error: ${err}`);
+  });
 });
 
 async function getCode(roomId) {
-  return new Promise((resolve, reject) => {
-    client
+  return new Promise((resolve, reject) => redisHandler.then(async (client) => {
+    await client
       .multi()
       .hget(roomId, 'code')
       .expire(roomId, EXPIRE_TIME)
@@ -23,12 +27,13 @@ async function getCode(roomId) {
           resolve(result[0]);
         }
       });
-  });
+    client.unref();
+  }));
 }
 
 async function updateCode(roomId, code) {
-  return new Promise((resolve, reject) => {
-    client
+  return new Promise((resolve, reject) => redisHandler.then(async (client) => {
+    await client
       .multi()
       .hmset(roomId, ['code', code, 'updateTime', Date.now()])
       .expire(roomId, EXPIRE_TIME)
@@ -39,11 +44,16 @@ async function updateCode(roomId, code) {
           resolve(result[0]);
         }
       });
-  });
+    client.unref();
+  }));
 }
 
 async function roomExistQuery(roomId) {
-  return exists(roomId);
+  return new Promise((resolve) => redisHandler.then(async (client) => {
+    const exists = promisify(client.exists).bind(client);
+    await resolve(exists(roomId));
+    client.unref();
+  }));
 }
 
 module.exports = { updateCode, getCode, roomExistQuery };
