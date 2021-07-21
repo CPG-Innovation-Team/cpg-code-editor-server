4d57aebe3312e8ebeae558358b691782771c3f60
casualbill <hotwindzb@gmail.com>
2021-07-21 20:19:31 +0800

    Update project data structure in db operation

--- mongodb.js
+++ mongodb.js
@@ -19,37 +19,41 @@ const queryProjectById = async (projectId) => {
   return result;
 };
 
-const createProject = async (projectId, syntax) => {
+const createProject = async (projectId, productName, syntax) => {
   const projectCollection = db.collection('project');
   const createTime = Date.now();
   const result = await projectCollection.insertOne({
     id: projectId,
+    productName,
     code: '',
     createTime,
     updateTime: createTime,
     syntax,
     relatedUser: [],
+    available: true,
   });
   return result;
 };
 
-const updateProject = async (projectId, code, syntax) => {
+const updateProject = async (projectId, data) => {
   const projectCollection = db.collection('project');
   const updateTime = Date.now();
   const result = await projectCollection.updateOne(
     { id: projectId },
     {
-      code,
       updateTime,
-      syntax,
+      ...data,
     },
   );
   return result;
 };
+
+const removeProject = (projectId) => updateProject(projectId, { available: false });
 
 module.exports = {
   queryProjectList,
   queryProjectById,
   createProject,
   updateProject,
+  removeProject,
 };
