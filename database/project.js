5c44620815b0b0147cab8517d3712b00d86cb8b8
casualbill <hotwindzb@gmail.com>
2021-07-22 20:28:56 +0800

    Extract reusable constant in project db operation

--- database/project.js
+++ database/project.js
@@ -1,21 +1,18 @@
 const db = require('./mongodb');
 
-const COLLECTION_PROJECT_INFO = 'project_info';
+const projectCollection = db.collection('project_info');
 
 const dbFindProject = async (param) => {
-  const projectCollection = db.collection(COLLECTION_PROJECT_INFO);
   const result = await projectCollection.find(param).toArray();
   return result;
 };
 
 const dbInsertProject = async (param) => {
-  const projectCollection = db.collection(COLLECTION_PROJECT_INFO);
   const result = await projectCollection.insertOne({ ...param });
   return result;
 };
 
 const dbUpdateProject = async (queryParam, data) => {
-  const projectCollection = db.collection(COLLECTION_PROJECT_INFO);
   const result = await projectCollection.updateOne(queryParam, data);
   return result;
 };
