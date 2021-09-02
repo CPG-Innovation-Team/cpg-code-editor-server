7a5ce88211f316f70584cee598c37e16f52693db
casualbill <hotwindzb@gmail.com>
2021-09-02 22:09:50 +0800

    Set different node env for dev and prod

--- database/mongodb.js
+++ database/mongodb.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const DB_URL = process.env.NODE_ENV === 'test' ? process.env.MONGO_URL : 'mongodb://localhost:27017';
+const DB_URL = process.env.MONGO_URL || 'mongodb://localhost:27017';
 const DB_NAME = 'cpg_code_editor';
 
 const client = new MongoClient(DB_URL);
